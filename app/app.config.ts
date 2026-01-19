2da340a90df9c946bf7190bfe07834b4972b15ae
ryuhei373 <mail@ryuhei.dev>
2026-01-20 00:17:19 +0900

    test: lefthook動作確認

--- app/app.config.ts
+++ app/app.config.ts
@@ -14,6 +14,9 @@ export default defineAppConfig({
       code: {
         base: 'px-1.5 py-0.5 text-sm font-mono font-medium rounded-md inline-block break-words max-w-full',
       },
+      p: {
+        base: 'my-5 leading-7', // text-prettyを削除（iOS Safariで折り返しが不安定になる問題）
+      },
     },
     page: {
       slots: {
